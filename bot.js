a81aff6f545e4507486d453f23b96aedb14e7c9c
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-11-16 19:21:29 +0100

    kicking user

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ client.on('ready', () => {
   console.log('bot is connected...');
 });
 
+const PREFIX = '$';
+
 client.on('message', msg => {
   if (msg.channel.id === '776449477757632535' && msg.content === 'naber') {
     // reply with mention
@@ -15,5 +17,29 @@ client.on('message', msg => {
 
     // reply without mention
     // msg.channel.send('')
+  }
+
+  if (message.author.bot) return;
+
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    if (CMD_NAME === 'kick') {
+      if (!message.member.hasPermission('KICK_MEMBERS')) {
+        return message.reply('You do not have permissions to use that command');
+      }
+      if (args.length === 0) return message.reply('Please provide an ID');
+      const member = message.guild.members.cache.get(args[0]);
+      if (member) {
+        member
+          .kick()
+          .then(member => message.channel.send(`{member} was kicked!`))
+          .catch(err => message.channel.send('I do not have permissions :('));
+      } else {
+        message.channel.send('That member was not found.');
+      }
+    }
   }
 });
